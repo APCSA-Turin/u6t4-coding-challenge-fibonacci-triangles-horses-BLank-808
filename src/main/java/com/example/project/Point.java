d3b7ea98089d40f19f1a14ebd9d0d9a1a5d8dd89
Brayan Ajpop <ajpop218@gmail.com>
2025-01-06 23:06:00 +0000

    passed all tests

--- src/main/java/com/example/project/Point.java
+++ src/main/java/com/example/project/Point.java
@@ -1,22 +1,39 @@
 package com.example.project;
-
 public class Point {
     private int x;
     private int y;
-    
+   
     public Point(int x, int y) {
       this.x = x;
       this.y = y;
     }
-    
+
+    public int getX(){
+      return x;
+    }
+
+    public int getY(){
+      return y;
+    }
+
+
     // Returns distance between this Point object and another Point object
     public double distanceTo(Point other) {
-      /* IMPLEMENT ME */
+        double a=Math.pow((x-other.x),2.0);
+        double b=Math.pow((y-other.y),2.0);
+       
+      return (Math.sqrt(a+b));
     }
-  
+ 
     // Returns a string in the format: (x, y)
     public String pointInfo() {
-      /* IMPLEMENT ME */
+      return "("+x+", "+y+")";
     }
-  }
-  
+
+
+    public void shift(int amount){
+      x+=amount;
+      y+=amount;
+    }
+
+}
--- src/main/java/com/example/project/Triangle.java
+++ src/main/java/com/example/project/Triangle.java
@@ -3,24 +3,50 @@ package com.example.project;
 public class Triangle {
     // array of Point objects
     private Point[] vertices;
-    
+   
     // Constructor: initializes the vertices array
     // to contain the three Point objects
     public Triangle(Point p1, Point p2, Point p3) {
-      /* IMPLEMENT ME */
+      vertices=new Point[] {p1,p2,p3};
     }
-    
+   
     // Returns the perimeter of the Triangle
     // HINT: use the distanceTo method that you wrote in the Point class
     public double perimeter() {
-      /* IMPLEMENT ME */
+      double perimeter=0;
+
+      for(int i =1; i<vertices.length;i++){
+        perimeter += vertices[i-1].distanceTo(vertices[i]);
+        if(i+1 == vertices.length){
+          perimeter += vertices[2].distanceTo(vertices[0]);
+          break;
+        }
+      }
+      return perimeter;
     }
-  
+ 
     // Returns a String with the three vertices that make up the Triangle;
     // if the vertices are the points (6, 10), (11, 15), and (18, 7),
     // this method should return: "[(6, 10), (11, 15), (18, 7)]"
     public String triangleInfo() {
-      /* IMPLEMENT ME */
+      String str = "[";
+     
+      for (Point p: vertices) {
+        str+= p.pointInfo()+", ";
+      }
+      str=str.substring(0, str.length()-2);
+      str += "]";
+      return str;
+    }
+
+
+    public void shift(int amount){
+      for (int index = 0; index < vertices.length; index++) {
+        vertices[index].shift(amount);
+      }
+    }
+
+    public Point[] getVertices(){
+      return vertices;
     }
   }
-  
